a3846c885d8af350edf0ddf31eb4d6953b046588
AnangaAmanta <ananga.amanta@gmail.com>
2024-10-18 14:38:46 +0530

    first commit

---
+++ reactHandsOn/src/main/java/snfl/restapi/reactHandsOn/ReactHandsOnApplication.java
@@ -0,0 +1,13 @@
+package snfl.restapi.reactHandsOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactHandsOnApplication.class, args);
+	}
+
+}
---
+++ reactHandsOn/src/test/java/snfl/restapi/reactHandsOn/ReactHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package snfl.restapi.reactHandsOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
